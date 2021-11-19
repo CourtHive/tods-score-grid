33b252a6bd0580f8d1c80386279bea6973c3db30
CourtHive <charles@courthive.com>
2021-11-19 11:31:07 +0100

    add viewport

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,11 @@ module.exports = {
   features: {
     buildStoriesJson: true
   },
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-dark-mode', '@storybook/addon-actions']
+  addons: [
+    '@storybook/addon-viewport',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-dark-mode',
+    '@storybook/addon-actions'
+  ]
 };
