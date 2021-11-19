948ca3c380ca5c5ae561ab0f3c7cbe9522859bf1
CourtHive <charles@courthive.com>
2021-11-19 06:33:12 +0100

    ITF winnerChevron

--- src/components/Side/Side.jsx
+++ src/components/Side/Side.jsx
@@ -2,12 +2,18 @@ import { Individual } from './Individual';
 import { SideScore } from './SideScore';
 import { css } from '@stitches/react';
 import React from 'react';
+import { CenterInfo } from './CenterInfo';
+
+const sideContainerStyle = css({
+  display: 'flex',
+  flexDirection: 'column'
+});
 
 const participantTypeStyle = css({
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'space-between',
-  marginRight: '1rem',
+  marginRight: '0rem',
   overflowX: 'scroll',
   scrollbarWidth: 'none',
   fontWeight: 500,
@@ -34,15 +40,16 @@ export const Side = ({
   score,
   sides
 }) => {
-  const sideContainerStyle = css({
+  const participantContainerStyle = css({
     display: 'flex',
     position: 'relative',
     height: participantHeight,
     boxSizing: 'border-box',
+    minWidth: '15rem',
     alignItems: 'center',
-    backgroundColor: '$backgroundColor',
+    backgroundColor: '$matchUp',
     justifyContent: 'space-between',
-    padding: '0.5rem 0.5rem 0.5rem 0.75rem',
+    padding: '0.5rem 0rem 0.5rem 0.75rem',
     '& p': {
       fontFamily: 'Sharp Sans, Arial, sans-serif',
       color: '$color'
@@ -59,16 +66,40 @@ export const Side = ({
   const isTeam = matchUpType === 'TEAM';
   const isDoubles = matchUpType === 'DOUBLES';
   const side = sides?.find((side) => side.sideNumber === sideNumber);
-  const firstParticipant = isDoubles ? side.participant?.individualParticipants?.[0] : side.participant;
+  const firstParticipant = isDoubles ? side.participant?.individualParticipants?.[0] : side?.participant;
   const secondParticipant = isDoubles && side.participant?.individualParticipants?.[1];
   const isWinningSide = sideNumber === winningSide;
+  const winnerChevron = composition?.winnerChevron && isWinningSide;
   //   const drawPosition = side?.drawPosition;
 
+  const chevronHeight = (isDoubles ? 0.35 : 0.3) * participantHeight;
+  const chevronStyle = css({
+    variants: {
+      variant: {
+        winner: {
+          '&:before': {
+            content: '',
+            position: 'absolute',
+            display: 'block',
+            borderTop: `${chevronHeight}px solid transparent`,
+            borderBottom: `${chevronHeight}px solid transparent`,
+            borderLeft: '8px solid #008f70',
+            left: 0
+          }
+        }
+      }
+    }
+  });
+
+  const centerInfo = composition?.centerInfo;
+
   return (
-    <div
-      className={sideContainerStyle({
-        sideNumber
-        /*
+    <div className={sideContainerStyle()}>
+      {!centerInfo || sideNumber === 1 ? null : <CenterInfo height={30} sideNumber={sideNumber} />}
+      <div
+        className={participantContainerStyle({
+          sideNumber
+          /*
         css: {
           '&:before': {
             position: 'absolute',
@@ -77,35 +108,37 @@ export const Side = ({
           }
         }
         */
-      })}
-    >
-      <div className={participantTypeStyle({ variant: isDoubles ? 'doubles' : undefined })}>
-        {isTeam ? null : (
-          <>
-            <Individual
-              individualParticipant={firstParticipant}
-              isWinningSide={isWinningSide}
-              composition={composition}
-              side={side}
-            />
-            {!secondParticipant ? null : (
+        })}
+      >
+        <div className={participantTypeStyle({ variant: isDoubles ? 'doubles' : undefined })}>
+          {isTeam ? null : (
+            <div className={chevronStyle({ variant: winnerChevron ? 'winner' : undefined })}>
               <Individual
-                individualParticipant={secondParticipant}
+                individualParticipant={firstParticipant}
                 isWinningSide={isWinningSide}
                 composition={composition}
                 side={side}
               />
-            )}
-          </>
-        )}
-      </div>{' '}
-      <SideScore
-        matchUpStatus={matchUpStatus}
-        composition={composition}
-        winningSide={winningSide}
-        sideNumber={sideNumber}
-        score={score}
-      />
+              {!secondParticipant ? null : (
+                <Individual
+                  individualParticipant={secondParticipant}
+                  isWinningSide={isWinningSide}
+                  composition={composition}
+                  side={side}
+                />
+              )}
+            </div>
+          )}
+        </div>{' '}
+        <SideScore
+          matchUpStatus={matchUpStatus}
+          composition={composition}
+          winningSide={winningSide}
+          sideNumber={sideNumber}
+          score={score}
+        />
+      </div>
+      {!centerInfo || sideNumber === 2 ? null : <CenterInfo height={30} sideNumber={sideNumber} />}
     </div>
   );
 };
