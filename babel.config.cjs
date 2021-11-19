60b9efc7cd3244dd8699a39c70f715f63023512d
CourtHive <charles@courthive.com>
2021-11-19 08:08:31 +0100

    cleanup; silence babel warnings;

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,9 +1,12 @@
 module.exports = {
-  presets: ['@babel/preset-typescript', ['@babel/preset-env', { targets: { node: 'current' } }]],
+  presets: ['@babel/preset-env'],
   plugins: [
     // Adds syntax support for optional chaining (.?)
     '@babel/plugin-proposal-optional-chaining',
     // Adds syntax support for default value using ?? operator
-    '@babel/plugin-proposal-nullish-coalescing-operator'
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }]
   ]
 };
