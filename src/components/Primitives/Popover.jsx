4b5a68a5b40b37b14f4eff0113989b17c3e18cef
CourtHive <charles@courthive.com>
2021-11-28 17:55:22 +0100

    popover testing

---
+++ src/components/Primitives/Popover.jsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import { styled, keyframes } from '@stitches/react';
+import { violet, blackA } from '@radix-ui/colors';
+import { MixerHorizontalIcon, Cross2Icon } from '@radix-ui/react-icons';
+import * as PopoverPrimitive from '@radix-ui/react-popover';
+import Dropzone, { useDropzone } from 'react-dropzone';
+
+const slideUpAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateY(2px)' },
+  '100%': { opacity: 1, transform: 'translateY(0)' }
+});
+
+const slideRightAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateX(-2px)' },
+  '100%': { opacity: 1, transform: 'translateX(0)' }
+});
+
+const slideDownAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateY(-2px)' },
+  '100%': { opacity: 1, transform: 'translateY(0)' }
+});
+
+const slideLeftAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateX(2px)' },
+  '100%': { opacity: 1, transform: 'translateX(0)' }
+});
+const StyledContent = styled(PopoverPrimitive.Content, {
+  borderRadius: 4,
+  padding: 20,
+  width: 260,
+  backgroundColor: 'white',
+  boxShadow: 'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
+  '@media (prefers-reduced-motion: no-preference)': {
+    animationDuration: '400ms',
+    animationTimingFunction: 'cubic-bezier(0.16, 1, 0.3, 1)',
+    willChange: 'transform, opacity',
+    '&[data-state="open"]': {
+      '&[data-side="top"]': { animationName: slideDownAndFade },
+      '&[data-side="right"]': { animationName: slideLeftAndFade },
+      '&[data-side="bottom"]': { animationName: slideUpAndFade },
+      '&[data-side="left"]': { animationName: slideRightAndFade }
+    }
+  },
+  '&:focus': {
+    boxShadow: `hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px, 0 0 0 2px ${violet.violet7}`
+  }
+});
+
+const StyledArrow = styled(PopoverPrimitive.Arrow, {
+  fill: 'white'
+});
+
+const StyledClose = styled(PopoverPrimitive.Close, {
+  all: 'unset',
+  fontFamily: 'inherit',
+  borderRadius: '100%',
+  height: 25,
+  width: 25,
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: violet.violet11,
+  position: 'absolute',
+  top: 5,
+  right: 5,
+
+  '&:hover': { backgroundColor: violet.violet4 },
+  '&:focus': { boxShadow: `0 0 0 2px ${violet.violet7}` }
+});
+
+// Exports
+export const Popover = PopoverPrimitive.Root;
+export const PopoverTrigger = PopoverPrimitive.Trigger;
+export const PopoverContent = StyledContent;
+export const PopoverArrow = StyledArrow;
+export const PopoverClose = StyledClose;
+
+// Your app...
+const Flex = styled('div', { display: 'flex' });
+
+const IconButton = styled('button', {
+  all: 'unset',
+  fontFamily: 'inherit',
+  borderRadius: '100%',
+  height: 35,
+  width: 35,
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: violet.violet11,
+  backgroundColor: 'white',
+  boxShadow: `0 2px 10px ${blackA.blackA7}`,
+  '&:hover': { backgroundColor: violet.violet3 },
+  '&:focus': { boxShadow: `0 0 0 2px black` }
+});
+
+const PopoverDemo = () => {
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <IconButton aria-label="Update dimensions">
+          <MixerHorizontalIcon />
+        </IconButton>
+      </PopoverTrigger>
+      <PopoverContent sideOffset={5}>
+        <Flex css={{ flexDirection: 'column', gap: 10 }}>
+          <section className="container">
+            <div {...getRootProps({ className: 'dropzone' })}>
+              <input {...getInputProps()} />
+              <p>Drag / drop or click to select files</p>
+            </div>
+            <aside>
+              <h4>Files</h4>
+              <ul>
+                {acceptedFiles.map((file) => (
+                  <li key={file.path}>
+                    {file.path} - {file.size} bytes
+                  </li>
+                ))}
+              </ul>
+            </aside>
+          </section>
+        </Flex>
+        <PopoverArrow />
+        <PopoverClose aria-label="Close">
+          <Cross2Icon />
+        </PopoverClose>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default PopoverDemo;
