c027bb419e40c62e305b9bfd71d03d4f8a62c53f
courthive <charles@courthive.com>
2023-07-10 12:06:08 -0400

    add chained conditional for missing side

--- src/components/Side/Side.jsx
+++ src/components/Side/Side.jsx
@@ -101,7 +101,7 @@ export const Side = ({ participantHeight, eventHandlers, matchUp, composition, s
   const side = matchUp?.sides?.find((side) => side.sideNumber === sideNumber);
   const firstParticipant = isDoubles ? side?.participant?.individualParticipants?.[0] : side?.participant;
   const secondParticipant = isDoubles && side?.participant?.individualParticipants?.[1];
-  const isWinningSide = sideNumber === winningSide || (matchUpStatus === 'BYE' && side.participant);
+  const isWinningSide = sideNumber === winningSide || (matchUpStatus === 'BYE' && side?.participant);
   const winnerChevron = configuration?.winnerChevron && isWinningSide;
   const teamLogo = configuration?.teamLogo;
   const entryStatus = matchUp?.side?.participant?.entryStatus?.replace('_', ' ');
